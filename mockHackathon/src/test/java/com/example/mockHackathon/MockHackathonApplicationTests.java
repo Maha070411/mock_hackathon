c230608c6fb6dd12e22ee9828b75cc8d8922824c
CHAITHRA S <147913846+chaithrajaya@users.noreply.github.com>
2026-04-07 10:24:29 +0530

    Add files via upload

---
+++ mockHackathon/src/test/java/com/example/mockHackathon/MockHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockHackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockHackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
